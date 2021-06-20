60afa5b74fbfa6bf742e04c3c80c94831af9d155
Kamil Korczynski <korczas@gmail.com>
2021-06-20 19:58:35 +0200

    init my config

--- resources/scripts/setup_symlinks.sh
+++ resources/scripts/setup_symlinks.sh
@@ -16,7 +16,6 @@ ln -vsf ~/.repos/dotfiles/tmux ~/
 ln -vsf ~/.repos/dotfiles/vim/UltiSnips ~/.config/nvim/
 ln -vsf ~/.repos/dotfiles/vim/coc-settings.json ~/.config/nvim/coc-settings.json
 ln -vsf ~/.repos/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -vsf ~/.repos/dotfiles/jetbrains/ideavimrc ~/.ideavimrc
 ln -vsf ~/.repos/dotfiles/asdf/default-npm-packages ~/.default-npm-packages
 ln -vsf ~/.repos/dotfiles/asdf/default-gems ~/.default-gems
 ln -vsf ~/.repos/dotfiles/tmuxinator ~/
@@ -24,14 +23,12 @@ ln -vsf ~/.repos/dotfiles/tmuxinator ~/
 case "$(uname -s)" in
   Darwin)
     mkdir -p ~/Library/Application\ Support/Code/User
-    mkdir -p ~/.config/karabiner
 
-    ln -vsf ~/.repos/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-    ln -vsf ~/.repos/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-    ln -vsf ~/.repos/dotfiles/vscode/snippets ~/Library/Application\ Support/Code/User
+    #ln -vsf ~/.repos/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+    #ln -vsf ~/.repos/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+    #ln -vsf ~/.repos/dotfiles/vscode/snippets ~/Library/Application\ Support/Code/User
     ln -vsf ~/.repos/dotfiles/resources/osx/tmux.conf ~/.tmux.conf
     ln -vsf ~/.repos/dotfiles/resources/osx/rc.conf ~/.config/ranger/rc.conf
-    ln -vsf ~/.repos/dotfiles/resources/osx/karabiner.json ~/.config/karabiner/karabiner.json
     ;;
   Linux)
     ln -vsf ~/.repos/dotfiles/resources/linux/tmux.conf ~/.tmux.conf
