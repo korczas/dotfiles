c4f61a775cfd982e4c4fd9d7697e1559d765e52d
Kamil Korczynski <korczas@gmail.com>
2021-10-01 15:59:52 +0200

    feat: add p10k config file

--- scripts/setup_symlinks.sh
+++ scripts/setup_symlinks.sh
@@ -6,6 +6,7 @@ ln -vsf ~/.repos/dotfiles/asdf/.asdfrc ~/.asdfrc
 ln -vsf ~/.repos/dotfiles/git/.gitconfig ~/.gitconfig
 ln -vsf ~/.repos/dotfiles/git/.gitignore_global ~/.gitignore_global
 ln -vsf ~/.repos/dotfiles/zsh/.zshrc ~/.zshrc
+ln -vsf ~/.repos/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
 
 case "$(uname -s)" in
   Darwin)
