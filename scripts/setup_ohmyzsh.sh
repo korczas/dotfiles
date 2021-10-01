2c2dbc8efe91d303da93a1705ff598fb167afc96
Kamil Korczynski <korczas@gmail.com>
2021-10-01 16:04:13 +0200

    feat: add p10k to setup_ohmyzsh

--- scripts/setup_ohmyzsh.sh
+++ scripts/setup_ohmyzsh.sh
@@ -1,5 +1,6 @@
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/alias-tips
