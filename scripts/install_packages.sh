a127494219430b7587e655369e0c822111486b3c
Kamil Korczynski <korczas@gmail.com>
2023-11-08 23:50:44 +0100

    refactor(scripts): clean install_packages file

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,24 +1,3 @@
-#case "$(uname -s)" in
-#  Darwin)
-#      brew install the_silver_searcher neovim reattach-to-user-namespace tmux diff-so-fancy asdf gnupg coreutils curl git ripgrep git-delta imagemagick
-#      brew cask install alacritty spectacle flux slack karabiner-elements lunar whatsapp iterm2 goofy istat-menus alfred spotify contexts intel-power-gadget steam
-#      brew install --HEAD universal-ctags/universal-ctags/universal-ctags
-#    ;;
-#
-#  Linux)
-#      git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-#      cd ~/.asdf
-#      git checkout "$(git describe --abbrev=0 --tags)"
-#      cd -
-#      sudo add-apt-repository -y ppa:neovim-ppa/unstable
-#      sudo apt update -y
-#      sudo apt install -y gcc perl autoconf pkg-config curl wget zsh xclip \
-#        python-dev python-pip python3-dev python3-pip neovim \
-#        silversearcher-ag scrot tmux
-#    ;;
-#esac
-
-
 brew install \
     asdf \
     bat \
@@ -32,7 +11,7 @@ brew install \
     htop \
     imagemagick \
     mas \
-    pyenv \
+    reattach-to-user-namespace \
     ripgrep \
     tmux \
     zsh
@@ -44,14 +23,14 @@ brew install --cask \
     postman \
     qnapi \
     raycast \
+    slack \
     transmission \
     visual-studio-code \
     vlc
 
 asdf plugin add nodejs
 asdf plugin add golang
-
-pyenv install 3.12.0
+asdf plugin add python
 
 # mas - cli for mac app store
 mas lucky \
