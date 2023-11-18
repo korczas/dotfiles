23944c185df83cc908f29c4910b4b91ec922bb7a
Kamil Korczynski <korczas@gmail.com>
2023-11-18 01:29:05 +0100

    chore: update symlink file

--- scripts/setup_symlinks.sh
+++ scripts/setup_symlinks.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
 mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/tmux
 
 ln -vsf ~/.repos/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -vsf ~/.repos/dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
+
 ln -vsf ~/.repos/dotfiles/asdf/.default-npm-packages ~/.default-npm-packages
 ln -vsf ~/.repos/dotfiles/asdf/.asdfrc ~/.asdfrc
+
 ln -vsf ~/.repos/dotfiles/git/.gitconfig ~/.gitconfig
 ln -vsf ~/.repos/dotfiles/git/.gitignore_global ~/.gitignore_global
+
 ln -vsf ~/.repos/dotfiles/zsh/.zshrc ~/.zshrc
 ln -vsf ~/.repos/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
 ln -vsf ~/.repos/dotfiles/zsh/.zsh_custom ~/.zsh_custom
-
-case "$(uname -s)" in
-  Darwin)
-    # mkdir -p ~/Library/Application\ Support/Code/User
-    # ln -vsf ~/.repos/dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-
-    ln -vsf ~/.repos/dotfiles/tmux/tmux.conf ~/.tmux.conf
-    ;;
-
-  # Linux)
-  #   ln -vsf ~/.repos/dotfiles/resources/linux/tmux.conf ~/.tmux.conf
-esac
