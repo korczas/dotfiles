ac00be1f53c65853cb382a5325bb3fe76bb60f98
Kamil Korczynski <korczas@gmail.com>
2023-12-11 18:01:57 +0100

    feat(defaults): update default (might not work)

--- mac_defaults/setup_defaults.sh
+++ mac_defaults/setup_defaults.sh
@@ -18,7 +18,28 @@ defaults write com.apple.dock largesize -int 80
 killall Dock
 
 ## date format
-defaults write com.apple.menuextra.clock "DateFormat" -string "\"d MMM HH:mm:ss\""
+defaults write com.apple.menuextra.clock "DateFormat" -string "d MMM 'at' HH:mm:ss"
+defaults write com.apple.menuextra.clock "ShowSeconds" -int 1
+defaults write com.apple.menuextra.clock "ShowDate" -int 1
+defaults write com.apple.menuextra.clock "FlashDateSeparators" -int 0
+defaults write com.apple.menuextra.clock "ShowDayOfWeek" -int 0
+
+## always show scrollbar
+defaults write -g AppleShowScrollBars -string Always
+
+## control center - might not work
+### bt
+defaults write com.apple.controlcenter "NSStatusItem Visible Bluetooth" -int 1
+defaults write com.apple.controlcenter "NSStatusItem Preferred Position Bluetooth" -int 344
+
+### dnd
+defaults write com.apple.controlcenter "NSStatusItem Visible FocusModes" -int 1
+defaults write com.apple.controlcenter "NSStatusItem Preferred Position FocusModes" -int 376
+
+### sound
+defaults write com.apple.controlcenter "NSStatusItem Visible Sound" -int 1
+defaults write com.apple.controlcenter "NSStatusItem Preferred Position FocusModes" -int 376
+
 
 ## finder
 ### show hidden files by default
@@ -26,19 +47,19 @@ defaults write com.apple.Finder "AppleShowAllFiles" -bool TRUE
 ### enable "Quit" option
 # defaults write com.apple.finder "QuitMenuItem" -bool FALSE && killall Finder
 
-## menu bar
-### hide spotlight icon
-defaults write com.apple.Spotlight.MenuItemHidden -int 1
-### show percentage next to battery icon
-defaults write com.apple.controlcenter.BatteryShowPercentage -int 1
-### show bluetooth icon
-defaults write com.apple.controlcenter.Bluetooth -int 18
-### show dnd icon
-defaults write com.apple.controlcenter.DoNotDisturb -int 2
+# ## menu bar
+# ### hide spotlight icon
+# defaults write com.apple.Spotlight.MenuItemHidden -int 1
+# ### show percentage next to battery icon
+# defaults write com.apple.controlcenter.BatteryShowPercentage -int 1
+# ### show bluetooth icon
+# defaults write com.apple.controlcenter.Bluetooth -int 18
+# ### show dnd icon
+# defaults write com.apple.controlcenter.DoNotDisturb -int 2
 
-## screen saver
-defaults write com.apple.screensaver.idleTime -int 0
-defaults write com.apple.screensaver.lastDelayTime -int 1200
+# ## screen saver
+# defaults write com.apple.screensaver.idleTime -int 0
+# defaults write com.apple.screensaver.lastDelayTime -int 1200
 
 ## turn off accented characters when holding down a key - requires logout
 defaults write -g ApplePressAndHoldEnabled -bool false
